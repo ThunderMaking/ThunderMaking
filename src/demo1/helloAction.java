4ca9b8a138591cbfd3db35c940162ce689025ed0
thunderqs <thunderqs@163.com>
2018-01-05 16:20:07 +0800

    代码test

---
+++ src/demo1/helloAction.java
@@ -0,0 +1,27 @@
+package demo1;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class helloAction extends ActionSupport {
+	public String demo1(){
+		System.out.println("demo1----");
+		return SUCCESS;
+	}
+	public String demo2(){
+		System.out.println("demo2----");
+		return SUCCESS;
+	}
+	public String demo3(){
+		System.out.println("demo3----");
+		return SUCCESS;
+	}
+	public String demo4(){
+		System.out.println("demo4----");
+		return SUCCESS;
+	}
+	public String demo5(){
+		System.out.println("demo5----");
+		return SUCCESS;
+	}
+	
+}
